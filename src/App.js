8c315879af4cd92b70f94fcdba330ba260484a0b
mmkhan <mariatechfuture@gmail.com>
2020-08-11 00:36:17 -0400

    Created wrapper and carousel components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Wrapper from "./components/Wrapper"
+import './App.scss';
+import Carousel from "./components/Carousel"
+
+function App() {
+  return (
+    <Wrapper>
+      <Carousel/>
+    </Wrapper>
+  );
+}
+
+export default App;
---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.scss";
+
+
+function Wrapper(props) {
+    return (
+    <div className="wrapper">
+        {props.children}
+        </div>
+    )
+}
+
+export default Wrapper;
