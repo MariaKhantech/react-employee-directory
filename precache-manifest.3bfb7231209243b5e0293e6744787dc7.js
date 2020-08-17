54112269681819f2ae2da7c4defaa187b5255c07
mmkhan <mariatechfuture@gmail.com>
2020-08-16 23:45:38 -0400

    Updates

---
+++ precache-manifest.3bfb7231209243b5e0293e6744787dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a61e889c46c5cfbb53574fee6effec",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "cd7dd66a3b878507b80b",
+    "url": "/react-employee-directory/static/css/main.ce32de1d.chunk.css"
+  },
+  {
+    "revision": "e70ecb0e9403279f5cbe",
+    "url": "/react-employee-directory/static/js/2.0a8d7071.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.0a8d7071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd7dd66a3b878507b80b",
+    "url": "/react-employee-directory/static/js/main.fd93344e.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
